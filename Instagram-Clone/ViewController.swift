ecc78c79441836b92755901516001b268218db24
ahandal24 <61199977+ahandal24@users.noreply.github.com>
2021-08-13 22:45:11 -0400

    Initial Commit

---
+++ Instagram-Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram-Clone
+//
+//  Created by Amber Handal on 8/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
